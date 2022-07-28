46461ecacb170d3343dce82af5c5636aaaee0c3f
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2022-07-28 12:50:51 -0700

    Added partners section

--- src/scenes/Home/PartnersSection/index.jsx
+++ src/scenes/Home/PartnersSection/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faGoogle,
-  faApple,
-  faPiedPiperAlt,
-  faSlack,
-  faYCombinator,
-} from '@fortawesome/free-brands-svg-icons';
+import PartnerLogoBC from '../../../assets/images/PartnerLogoBC.png?as=webp';
+import PartnerLogoVictory from '../../../assets/images/PartnerLogoVictory.png?as=webp';
+import PartnerLogoReigningChamp from '../../../assets/images/PartnerLogoReigningChamp.png?as=webp';
+import PartnerLogoVanRunCo from '../../../assets/images/PartnerLogoVanRunCo.png?as=webp';
+import PartnerLogoRunForHope from '../../../assets/images/PartnerLogoRunForHope.png?as=webp';
+import PartnerLogoRunVan from '../../../assets/images/PartnerLogoRunVan.png?as=webp';
 
 const PartnersWrapper = styled.div`
   background-color: var(--colour-primary);
@@ -22,7 +20,7 @@ const PartnersTitle = styled.span`
   font-family: var(--font-primary);
   font-size: var(--text-xxl);
   font-weight: bold;
-  margin-bottom: 0.5em;
+  margin-bottom: 1em;
 `;
 
 const PartnerLogosWrapper = styled.div`
@@ -30,14 +28,18 @@ const PartnerLogosWrapper = styled.div`
   flex-direction: column;
   gap: 2rem;
 
-  font-size: var(--text-xl);
-  color: var(--colour-white);
   justify-content: center;
 
   @media (min-width: 768px) {
     flex-direction: row;
+    flex-wrap: wrap;
     gap: 4rem;
   }
+`;
+
+const PartnerLogo = styled.img`
+  height: 32px;
+  width: auto;
 `;
 
 function PartnersSection() {
@@ -45,11 +47,36 @@ function PartnersSection() {
     <PartnersWrapper>
       <PartnersTitle>Partners</PartnersTitle>
       <PartnerLogosWrapper>
-        <FontAwesomeIcon icon={faGoogle} />
-        <FontAwesomeIcon icon={faPiedPiperAlt} />
-        <FontAwesomeIcon icon={faApple} />
-        <FontAwesomeIcon icon={faSlack} />
-        <FontAwesomeIcon icon={faYCombinator} />
+        <div>
+          <PartnerLogo src={PartnerLogoBC} alt="Partner Logo - BC" />
+        </div>
+        <div>
+          <PartnerLogo
+            src={PartnerLogoVictory}
+            alt="Partner Logo - Victory Co"
+          />
+        </div>
+        <div>
+          <PartnerLogo
+            src={PartnerLogoReigningChamp}
+            alt="Partner Logo - Reigning Champ"
+          />
+        </div>
+        <div>
+          <PartnerLogo
+            src={PartnerLogoVanRunCo}
+            alt="Partner Logo - Vancouver Running Company"
+          />
+        </div>
+        <div>
+          <PartnerLogo
+            src={PartnerLogoRunForHope}
+            alt="Partner Logo - Run 4 Hope"
+          />
+        </div>
+        <div>
+          <PartnerLogo src={PartnerLogoRunVan} alt="Partner Logo - Run Van" />
+        </div>
       </PartnerLogosWrapper>
     </PartnersWrapper>
   );
